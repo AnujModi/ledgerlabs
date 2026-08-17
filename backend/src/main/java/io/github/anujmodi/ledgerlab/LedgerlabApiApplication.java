1fea281df04d5dfb9c8cedbac999574d51de110b
AnujModi <amgsmodi@gmail.com>
2026-08-16 23:49:51 -0400

    build: bootstrap Spring Boot backend

---
+++ backend/src/main/java/io/github/anujmodi/ledgerlab/LedgerlabApiApplication.java
@@ -0,0 +1,11 @@
+package io.github.anujmodi.ledgerlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerlabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerlabApiApplication.class, args);	}
+}
